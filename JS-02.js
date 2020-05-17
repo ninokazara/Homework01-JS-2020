59223557b18e6f76a7a0048ee81b7d844e71adfd
nino <nino.kazarashvili15b@geolab.edu.ge>
2020-05-17 17:03:01 +0400

    lessson-02/ homework-02 object

--- JS-02.js
+++ JS-02.js
@@ -8,22 +8,76 @@ Date: 16.05.2020
 
 // object
 
-let car= {
+let car1 = {
     name: "Audi",
     color: "white",
     model:"A4",
     year: 2019,
     horsepower: 190,
-    price: 20000
+    price: 10000
 };
-cars = [car];
-for(let i=0; i<cars.length; i++) {
-    console.log(`This car has the following features: ${cars[i].name}`);
-    console.log(`This car has the following features: ${cars[i].color}`);
-    console.log(`This car has the following features: ${cars[i].model}`);
-    console.log(`This car has the following features: ${cars[i].year}`);
-    console.log(`This car has the following features: ${cars[i].horsepower}`);
-    console.log(`This car has the following features: ${cars[i].price}`);
 
+let car2 = {
+    name: "BMW",
+    color: "blue",
+    model:"X7",
+    year: 2019,
+    horsepower: 200,
+    price: 30000
+};
+
+let car3 = {
+    name: "Mercedes-benz",
+    color: "red",
+    model:"CLS",
+    year: 2018,
+    horsepower: 250,
+    price: 18000
+};
+
+let car4 = {
+    name: "Ford",
+    color: "black",
+    model:"mustang",
+    year: 2019,
+    horsepower: 300,
+    price: 35000
+};
+
+let cars = []
+
+cars.push(car1);
+cars.push(car2);
+cars.push(car3);
+cars.push(car4);
+
+let cheapcars = [];
+// let expensivecars = [];
+for(let i = 0; i < cars.length; i++) {
+    if(cars[i].price < 15000) {
+        cheapcars.push(cars[i]);
+    } else {
+        console.log('Unfortunately you can’t buy a car at this price');
+    }
 }
+
+console.log(cheapcars);
+// console.log(expensivecars);
+
+
+
+
+
+
+
+// cars = [car];
+// for(let i=0; i<cars.length; i++) {
+//     console.log(`This car has the following features: ${cars[i].name}`);
+//     console.log(`This car has the following features: ${cars[i].color}`);
+//     console.log(`This car has the following features: ${cars[i].model}`);
+//     console.log(`This car has the following features: ${cars[i].year}`);
+//     console.log(`This car has the following features: ${cars[i].horsepower}`);
+//     console.log(`This car has the following features: ${cars[i].price}`);
+
+// }
 
